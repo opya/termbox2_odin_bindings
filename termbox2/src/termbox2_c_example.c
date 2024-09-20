57912b851ba078812005389517212deb6aebbe48
Simeon Angelov <simeon.angelov@tuta.io>
2024-09-20 09:12:47 +0300

    add same example as in termbox2 project

--- termbox2/src/termbox2_c_example.c
+++ termbox2/src/termbox2_c_example.c
@@ -1,36 +1,28 @@
 #include <stdio.h>
 
 #define TB_IMPL
-#define TB_OPT_ATTR_W 64
 #include "./termbox2.h"
 
-#define BACKGROUND_GOLOR 0x00212121
-
-int main(void)
-{
-    int a = 0;
+int main(int argc, char **argv) {
     struct tb_event ev;
+    int y = 0;
 
-    if (tb_init() < 0) {
-        printf("Failed to tb_init().\n");
-        return 1;
-    }
+    tb_init();
 
-    tb_set_output_mode(TB_OUTPUT_TRUECOLOR);
-
-    for (int i =0; i < tb_width(); i++) {
-        for (int j =0; j < tb_height(); j++) {
-            tb_set_cell(i, j, ' ', 0, BACKGROUND_GOLOR);
-        }
-    }
-
-    tb_printf(0, a++, TB_GREEN, 0, "hello from termbox width - %d height -> %d", tb_width(), tb_height());
+    tb_printf(0, y++, TB_GREEN, 0, "hello from termbox");
+    tb_printf(0, y++, 0, 0, "width=%d height=%d", tb_width(), tb_height());
+    tb_printf(0, y++, 0, 0, "press any key...");
     tb_present();
+
     tb_poll_event(&ev);
 
-    if(tb_shutdown() < 0) {
-        printf("Failed to tb_shutdown().\n");
-    }
+    y++;
+    tb_printf(0, y++, 0, 0, "event type=%d key=%d ch=%c", ev.type, ev.key, ev.ch);
+    tb_printf(0, y++, 0, 0, "press any key to quit...");
+    tb_present();
+
+    tb_poll_event(&ev);
+    tb_shutdown();
 
     return 0;
 }
